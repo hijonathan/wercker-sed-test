92c8016166eecf4c7577df773628451cc27002bf
Jonathan K <hijonathan@users.noreply.github.com>
2015-12-13 16:37:38 -0500

    Create create-env.js

---
+++ create-env.js
@@ -0,0 +1,54 @@
+var fs = require('fs'),
+    outputDir = process.env.OUTPUT_DIR || './__',
+    env,
+    envStr,
+    fn;
+
+try {
+    env = JSON.parse(process.env.ENV_JSON);
+} catch (e) {
+    env = {};
+}
+
+envStr = JSON.stringify(env);
+
+fn = "(function (root, factory) {\n\
+    if (typeof define === 'function' && define.amd) {\n\
+        // AMD. Register as an anonymous module.\n\
+        define([''], function () {\n\
+            return (root.__env = factory());\n\
+        });\n\
+    } else if (typeof exports === 'object') {\n\
+        // Node. Does not work with strict CommonJS, but\n\
+        // only CommonJS-like enviroments that support module.exports,\n\
+        // like Node.\n\
+        module.exports = factory();\n\
+    } else {\n\
+        // Browser globals\n\
+        root.__env = factory();\n\
+    }\n\
+}(this, function () {\n\
+    return ENV;\n\
+}));"
+
+// Create the dir if it doesn't exist.
+fs.stat(outputDir, function(err, stat) {
+    if (err || stat == null) {
+        fs.mkdir(outputDir, createFiles)
+    } else {
+        createFiles()
+    }
+})
+
+function createFiles(err) {
+    if (err) throw err;
+
+    // Create json file.
+    fs.writeFileSync(outputDir + '/env.json', envStr);
+
+    // Create js file.
+    fs.writeFileSync(outputDir + '/env.js', fn.replace(/ENV/, envStr));
+
+    // Exit.
+    process.exit()
+}
